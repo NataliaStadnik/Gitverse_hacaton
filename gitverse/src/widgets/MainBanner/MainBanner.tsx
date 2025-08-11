f184cc2048bffb7c07f04ca67a61b97837f2241b
Natalia Stadnik <nata.stadnik.95@inbox.ru>
2025-08-12 00:08:42 +0300

    change banner animation

--- gitverse/src/widgets/MainBanner/MainBanner.tsx
+++ gitverse/src/widgets/MainBanner/MainBanner.tsx
@@ -1,46 +1,103 @@
 import {Button} from '@/shared'
 import './style.scss'
-import {useEffect, useState} from 'react'
-import banner1 from '@/assets/images/banner-1.svg'
-import banner2 from '@/assets/images/banner-2.svg'
-import banner3 from '@/assets/images/banner-3.svg'
+import {useRef, useEffect, JSX} from 'react'
+import {GlobeProduct, GProduct, ScProduct} from '@/assets/svg'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {Autoplay, EffectFade, Pagination} from 'swiper/modules'
+import 'swiper/css'
 
-const backgrounds = [banner1, banner2, banner3]
+type ContentBannerType = {
+  title: string[]
+  text: string
+  svg: (isHover: string) => JSX.Element
+}
+
+const contentBanner: ContentBannerType[] = [
+  {
+    title: ['SmartClass', 'для ИТ-команд'],
+    text: 'Ваш цифровой наставник для новых сотрудников',
+    svg: (isHover: string) => <ScProduct classes={isHover} />,
+  },
+  {
+    title: ['Разработка без границ', 'с CodeIDE'],
+    text: 'Java, Kotlin, Python и ещё 70 инструментов',
+    svg: (isHover: string) => <GProduct classes={isHover} />,
+  },
+  {
+    title: ['Создавайте и развивайте', 'проекты с AI-помощником'],
+    text: 'Ваш умный партнёр в коде, идеях и автоматизации',
+    svg: (isHover: string) => <GlobeProduct classes={isHover} />,
+  },
+]
 
 const MainBanner = () => {
-  const [index, setIndex] = useState(0)
+  const swiperRef = useRef<any>(null)
+  const delay = 1000
+  const duration = 600
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setIndex((prev) => (prev + 1) % backgrounds.length)
-    }, 4000)
-    return () => clearInterval(timer)
+    if (swiperRef.current) {
+      swiperRef.current.swiper.autoplay.stop()
+      setTimeout(() => {
+        swiperRef.current.swiper.autoplay.start()
+      }, delay)
+    }
   }, [])
 
+  const handleMouseEnter = () => {
+    swiperRef.current?.swiper?.autoplay?.pause()
+  }
+
+  const handleMouseLeave = () => {
+    swiperRef.current?.swiper?.autoplay?.resume()
+  }
+
   return (
-    <section className="main-banner">
+    <section
+      className="main-banner"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="wrapper">
-        <div className="main-banner__slider">
-          {backgrounds.map((src, i) => (
-            <img
-              key={src}
-              src={src}
-              alt={`background ${i}`}
-              className={`main-banner__image ${i === index ? 'active' : ''}`}
-            />
+        <Swiper
+          ref={swiperRef}
+          modules={[Autoplay, EffectFade, Pagination]}
+          effect="fade"
+          pagination={{
+            bulletClass: 'main-banner__bullet',
+            bulletActiveClass: 'main-banner__bullet--active',
+          }}
+          autoplay={{
+            delay: delay + duration,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+          }}
+          speed={duration}
+          loop
+          className="main-banner__slider"
+        >
+          {contentBanner.map((elem, i) => (
+            <SwiperSlide key={i}>
+              <div className="container main-banner__container">
+                <div className="main-banner__wrapper">
+                  <h2 className="title title--light">
+                    {elem.title.map((elem, i) => (
+                      <span key={i}> {elem}</span>
+                    ))}
+                  </h2>
+                  <p className="main-banner__text">{elem.text}</p>
+                  <Button text="Подробнее" linkTo="#" />
+                </div>
+
+                <div className="animation-banner">
+                  <div className="boble-big">
+                    {elem.svg('active')}
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
           ))}
-        </div>
-        <div className="container main-banner__container">
-          <div className="main-banner__wrapper">
-            <h2 className="title title--light">
-              Разработка без границ с CodeIDE
-            </h2>
-            <p className="main-banner__text">
-              Java, Kotlin, Python и ещё 70 инструментов
-            </p>
-            <Button text="Подробнее" linkTo="#" />
-          </div>
-        </div>
+        </Swiper>
       </div>
     </section>
   )
